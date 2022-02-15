fdb6ad6886487b64783d2aa94640bd0646c1c130
Dhairya Marwah <85454844+DhairyaMarwah@users.noreply.github.com>
2022-02-15 18:04:06 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route   } from "react-router-dom";
+import {Navbar} from './components';
+import './App.css'
 const App = () => {
   return (
      <>
      <div className="app">
          <div className="navbar">
-
+          <Navbar/>
          </div>
          <div className="main"></div>
          <div className="footer"></div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Button,Menu,Typography,Avatar} from 'antd'
-import Link from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import icon from '../images/cryptocurrency.png'
 const Navbar = () => {
   return (
     <div className="nav-container">
         <div className="logo-container">
-            <Avatar/>
+            <Avatar src={icon} size="large" />
             <Typography.Title level={2} className="logo">
                 <Link to="/">Cryptoverse</Link>
             </Typography.Title>
